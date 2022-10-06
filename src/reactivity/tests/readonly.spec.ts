7b4631ca80da86385438a6a94619f1c0b4facb11
guanjiameng <gjmhandsome@163.com>
2022-10-06 21:50:22 +0800

    feat:isProxy

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { isReadonly, readonly } from "../reactive";
+import { isReadonly, readonly, isProxy } from "../reactive";
 describe("readonly", () => {
   it("happy path", () => {
     // not set
@@ -8,6 +8,7 @@ describe("readonly", () => {
     expect(wrapped.foo).toBe(1);
     expect(isReadonly(wrapped)).toBe(true);
     expect(isReadonly(original)).toBe(false);
+    expect(isProxy(wrapped)).toBe(true);
   });
   it("warn when set target", () => {
     console.warn = jest.fn();
